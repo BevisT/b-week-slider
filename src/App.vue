5f54a4b3ea7a04544aa6c6dfe6e762fa0ac68165
scwn_bwy <2435773101@qq.com>
2019-06-06 15:28:03 +0800

    Bevis first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <week-slider
+			@dateClick="dateClickhandler"
+      todayTxtColor='#ff0000'
+			:showYear="true"
+			:defaultDate.sync="default_date">
+		</week-slider>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+
+    data () {
+        return {
+            default_date: '2019-06-06'
+        }
+    },
+    methods: {
+        dateClickhandler (date) {
+            console.log(date);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
